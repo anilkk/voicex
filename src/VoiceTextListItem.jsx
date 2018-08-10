8bbbbfa29f92679541bd7d238e8b16424fed30cb
Anil Kumar krishanshetty <anilbms75@gmail.com>
2018-08-10 14:54:04 +0200

    Fix bug edit of prompt was not updating.

--- src/VoiceTextListItem.jsx
+++ src/VoiceTextListItem.jsx
@@ -36,14 +36,21 @@ class VoiceTextListItem extends React.Component{
         const {voiceText, speech, remove, update,  index} = this.props;
 
         var className = (this.state.editModeClassName ? ' voice-text-item alignLeft voice-text-item-edit-mode ' : ' voice-text-item alignLeft ');
+        const updateText = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.onSave();
+            update(e.target.value, voiceText.id);
+        };
         // Each VoiceText
         return (<ListItem button className={className}>
                 <ListItemText className="voice-text-item-text" primary={voiceText.id + '.' + voiceText.text} onClick={() => {speech(voiceText.id)}} />
                 <EditIcon className="voice-text-edit-icon" onClick={this.goToEditMode}/>
                 <ClearIcon className="voice-text-clear-icon" onClick={this.onRemove} />
-                <EditVoiceInput voiceText={voiceText} update={update} blur={this.onSave}/>
+                <EditVoiceInput voiceText={voiceText} update={update} blur={updateText}/>
                 &ensp;&ensp;
-                <SaveIcon className="voice-text-save-icon" onClick={this.onSave} />
+                <SaveIcon className="voice-text-save-icon" onClick={updateText} />
+
             </ListItem>);
     }
 }
